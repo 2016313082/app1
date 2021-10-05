140dc616a5b31bbdaff80bfd704bdc58662ddbc3
Gustavo Vladimir Martínez Tamayo <2016313082@uteq.edu.mx>
2021-10-04 23:42:03 -0500

    * .gitignore: Creación de mi primer app
    * app1.sln:
    * App.xaml:
    * app1.csproj:
    * App.xaml.cs:
    * MainPage.xaml:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * MainActivity.cs:
    * app1.Android.csproj:
    * AboutAssets.txt:
    * AssemblyInfo.cs:
    * colors.xml:
    * styles.xml:
    * AboutResources.txt:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * icon_round.xml:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ app1/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app1
+{
+    public partial class MainPage : ContentPage
+    {
+        int _count;
+        string _message;
+        Button button1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _count = 0;
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            if (button1 == null)
+                button1 = ((Button)sender);
+            _count++;
+            _message = "Has hecho tab {0} veces";
+            button1.Text = string.Format(_message, _count);
+        }
+
+        private void Reset_Clicked(object sender, System.EventArgs e)
+        {
+            _message = "Haz tab en mi!";
+            if (button1 != null)
+                button1.Text = _message;
+            _count = 0;
+        }
+    }
+}
